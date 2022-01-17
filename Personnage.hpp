3497e73b5f96cfbde337055ca84e1cb8f8245bc1
OZYONUM arman <arman.ozyonum@etu.univ-paris-diderot.fr>
2022-01-17 13:41:12 +0100

    Personnage hpp

---
+++ Personnage.hpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Objet.hpp"
+
+using namespace std;
+
+#ifndef PROJET_PERSONNAGE_HPP
+#define PROJET_PERSONNAGE_HPP
+
+
+class Personnage {
+public:
+    vector<Objet*> inventaire;
+private:
+    string nom;
+    int sante;
+    int habilete{};
+    int posI{};
+    int posJ{};
+
+public:
+    const string &getNom() const;
+
+    void setNom(const string &nom);
+
+    int getSante() const;
+
+    void setSante(int sante);
+
+    int getHabilete() const;
+
+    void setHabilete(int habilete);
+
+    int getPosI() const;
+
+    void setPosI(int posI);
+
+    int getPosJ() const;
+
+    void setPosJ(int posJ);
+
+    const vector<Objet *> &getInventaire() const;
+};
+
+
+#endif //PROJET_PERSONNAGE_HPP
