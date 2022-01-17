0a50138b610389910b92c2100c6d7c3d4bf6f8d5
OZYONUM arman <arman.ozyonum@etu.univ-paris-diderot.fr>
2022-01-17 13:42:04 +0100

    Piece cpp

---
+++ Piece.cpp
@@ -0,0 +1,13 @@
+#include "Piece.hpp"
+
+bool Piece::aEscalier() {
+    return false;
+}
+
+const vector<struct Objet *> *const Piece::getObjets() const {
+    return Objets;
+}
+
+void Piece::enleverObjet(Objet *const &pObjet) {
+
+}
