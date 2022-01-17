8842e05b2298285f09ebb3acadac34ebd9fb66ab
OZYONUM arman <arman.ozyonum@etu.univ-paris-diderot.fr>
2022-01-17 13:40:33 +0100

    Personnage cpp

---
+++ Personnage.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include "Personnage.hpp"
+#include "Objet.hpp"
+
+const string &Personnage::getNom() const {
+    return nom;
+}
+
+void Personnage::setNom(const string &nom) {
+    Personnage::nom = nom;
+}
+
+int Personnage::getSante() const {
+    return sante;
+}
+
+void Personnage::setSante(int sante) {
+    Personnage::sante = sante;
+}
+
+int Personnage::getHabilete() const {
+    return habilete;
+}
+
+void Personnage::setHabilete(int habilete) {
+    Personnage::habilete = habilete;
+}
+
+int Personnage::getPosI() const {
+    return posI;
+}
+
+void Personnage::setPosI(int posI) {
+    Personnage::posI = posI;
+}
+
+int Personnage::getPosJ() const {
+    return posJ;
+}
+
+void Personnage::setPosJ(int posJ) {
+    Personnage::posJ = posJ;
+}
+
+const vector<Objet *> &Personnage::getInventaire() const {
+    return inventaire;
+}
