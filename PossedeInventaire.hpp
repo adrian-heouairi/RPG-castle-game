f5d4123ee283965aa068292c6e234d78d1b2428d
OZYONUM arman <arman.ozyonum@etu.univ-paris-diderot.fr>
2022-01-17 16:37:02 +0100

    Upload New File

---
+++ PossedeInventaire.hpp
@@ -0,0 +1,11 @@
+#ifndef PROJET_POSSEDEINVENTAIRE_HPP
+#define PROJET_POSSEDEINVENTAIRE_HPP
+
+
+class PossedeInventaire {
+public:
+    bool inventaireADeLaPlace();
+};
+
+
+#endif //PROJET_POSSEDEINVENTAIRE_HPP
