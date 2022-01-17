cc8b0e88e3583f307c4cc434dbf79c724f0a303a
OZYONUM arman <arman.ozyonum@etu.univ-paris-diderot.fr>
2022-01-17 17:25:04 +0100

    Update PossedeInventaire.hpp

--- PossedeInventaire.hpp
+++ PossedeInventaire.hpp
@@ -2,9 +2,20 @@
 #define PROJET_POSSEDEINVENTAIRE_HPP
 
 
+#include <vector>
+#include "Objet.hpp"
+
 class PossedeInventaire {
 public:
+    vector<Objet*> inventaire;
+
     bool inventaireADeLaPlace();
+
+    void ajouterObjet(Objet *const &pObjet);
+
+    void enleverObjet(Objet *const &pObjet);
+
+    const vector<Objet *> &getInventaire() const;
 };
 
 
