2b7b9ff51dbde3e49b80ffab10ec66f35804f99b
OZYONUM arman <arman.ozyonum@etu.univ-paris-diderot.fr>
2022-01-17 17:26:22 +0100

    Update PossedeInventaire.cpp

--- PossedeInventaire.cpp
+++ PossedeInventaire.cpp
@@ -1,9 +1,17 @@
-//
-// Created by Arman Ozyonum on 17.01.2022.
-//
-
 #include "PossedeInventaire.hpp"
 
 bool PossedeInventaire::inventaireADeLaPlace() {
     return false;
+}
+
+const vector<Objet *> &PossedeInventaire::getInventaire() const {
+    return inventaire;
+}
+
+void PossedeInventaire::ajouterObjet(Objet *const &pObjet) {
+
+}
+
+void PossedeInventaire::enleverObjet(Objet *const &pObjet) {
+
 }
