b4aae4dbbda36f76adc69b4aa7121084ec52988f
HEOUAIRI adrian <adrian.heouairi@etu.u-paris.fr>
2022-01-19 19:37:51 +0100

    Clé

--- CleDeTeleportation.cpp
+++ CleDeTeleportation.cpp
@@ -1,1 +1,30 @@
 #include "CleDeTeleportation.hpp"
+
+extern int maxI;
+extern int maxJ;
+extern int getRandomIntBetween(int a, int b);
+
+CleDeTeleportation::CleDeTeleportation() {
+    setNom("Clé de téléportation");
+}
+
+bool CleDeTeleportation::isUtilisableEnCombat(Personnage *joueur) {
+	return true;
+}
+
+void CleDeTeleportation::utiliserEnCombat(Personnage *joueur, Personnage *ennemi) {
+	utiliserHorsCombat(joueur);
+}
+
+bool CleDeTeleportation::isUtilisableHorsCombat(Personnage *joueur) {
+	return true;
+}
+
+void CleDeTeleportation::utiliserHorsCombat(Personnage *joueur) {
+	joueur->setPosI(getRandomIntBetween(0, maxI));
+    joueur->setPosJ(getRandomIntBetween(0, maxJ));
+}
+
+bool CleDeTeleportation::isConsommable() {
+	return true;
+}
--- CleDeTeleportation.hpp
+++ CleDeTeleportation.hpp
@@ -4,7 +4,18 @@
 #include "Objet.hpp"
 
 class CleDeTeleportation: public Objet{
+public:
+    CleDeTeleportation();
 
+    bool isUtilisableEnCombat(Personnage *joueur) override;
+
+    void utiliserEnCombat(Personnage *joueur, Personnage *ennemi) override;
+
+    bool isUtilisableHorsCombat(Personnage *joueur) override;
+
+    void utiliserHorsCombat(Personnage *joueur) override;
+
+    bool isConsommable() override;
 };
 
 #endif
