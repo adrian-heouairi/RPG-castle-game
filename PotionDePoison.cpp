23b9253abf338ee619b5f70c2a1a19a2cd313ecb
Arman <arman@ozyonum.com>
2022-01-19 18:21:32 +0100

    update

--- PotionDePoison.cpp
+++ PotionDePoison.cpp
@@ -1,2 +1,31 @@
+#include <iostream>
 #include "PotionDePoison.hpp"
+using namespace std;
 
+PotionDePoison::PotionDePoison(){
+    this->sante = 100;
+}
+
+bool isUtilisableEnCombat(Personnage *joueur){
+    return true;
+}
+
+string getNom(){
+    return "Potion De Poison";
+}
+
+void utiliserEnCombat(Personnage *joueur, Personnage *ennemi){
+    joueur->setSante(joueur->getSante() - 100);
+}
+
+bool isUtilisableHorsCombat(Personnage *joueur){
+    return true;
+}
+
+void utiliserHorsCombat(Personnage *joueur){
+    joueur->setSante(joueur->getSante() - 100);
+}
+    
+bool isConsommable(){
+    return false;
+}
--- PotionDePoison.hpp
+++ PotionDePoison.hpp
@@ -4,7 +4,22 @@
 #include "Objet.hpp"
 
 class PotionDePoison: public Objet{
+private:
+    int sante;
+public:
+    PotionDePoison();
 
+    bool isUtilisableEnCombat(Personnage *joueur);
+
+    string getNom();
+
+    void utiliserEnCombat(Personnage *joueur, Personnage *ennemi);
+
+    bool isUtilisableHorsCombat(Personnage *joueur);
+
+    void utiliserHorsCombat(Personnage *joueur);
+    
+    bool isConsommable();
 };
 
 #endif
