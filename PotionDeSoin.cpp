2187425d7152838e795e069b230d7b7b00bafe69
Arman <arman@ozyonum.com>
2022-01-19 17:10:43 +0100

    potiondesoin

--- PotionDeSoin.cpp
+++ PotionDeSoin.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 #include "PotionDeSoin.hpp"
+using namespace std;
+
+PotionDeSoin::PotionDeSoin(){
+    this->soin = 100;
+}
+
+bool isUtilisableEnCombat(Personnage *joueur){
+    return false;
+}
+
+string getNom(){
+    return "Potion De Soin";
+}
+
+void utiliserEnCombat(Personnage *joueur, Personnage *ennemi){
+    if (isUtilisableEnCombat(joueur)){
+        ennemi->setSante(ennemi->getSante() - 100);
+    }    
+}
+
+bool isUtilisableHorsCombat(Personnage *joueur){
+    return true;
+}
+
+void utiliserHorsCombat(Personnage *joueur){
+    if(isUtilisableHorsCombat){
+        joueur->setSante(joueur->getSante() + 100);
+    }
+}
+    
+bool isConsommable(){
+    return true;
+}
--- PotionDeSoin.hpp
+++ PotionDeSoin.hpp
@@ -1,10 +1,25 @@
 #ifndef POTIONDESOIN_HPP
 #define POTIONDESOIN_HPP
-
+#include <iostream>
 #include "Objet.hpp"
+using namespace std;
 
 class PotionDeSoin: public Objet{
+    int soin;
+public:
+    PotionDeSoin();
 
+    bool isUtilisableEnCombat(Personnage *joueur);
+
+    string getNom();
+
+    void utiliserEnCombat(Personnage *joueur, Personnage *ennemi);
+
+    bool isUtilisableHorsCombat(Personnage *joueur);
+
+    void utiliserHorsCombat(Personnage *joueur);
+    
+    bool isConsommable();
 };
 
 #endif
